545a333298aedbcd870d1ac49090185746651cef
VDowbensky <harbinger@ukr.net>
2026-02-05 15:40:31 +0200

    Merge branch 'meshtastic:develop' into develop

--- variants/esp32/rak11200/variant.h
+++ variants/esp32/rak11200/variant.h
@@ -6,8 +6,6 @@
 #define LED_GREEN 12
 #define LED_BLUE 2
 
-#define LED_BUILTIN LED_GREEN
-
 static const uint8_t TX = 1;
 static const uint8_t RX = 3;
 
@@ -52,7 +50,7 @@ static const uint8_t SCK = 33; //OK
 #undef GPS_TX_PIN
 #define GPS_TX_PIN (TX1)
 
-#define LED_PIN LED_BLUE
+#define LED_POWER LED_BLUE
 
 #define PIN_VBAT WB_A0
 #define BATTERY_PIN PIN_VBAT
